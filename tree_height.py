bba812070f3ba2638a496a4c9f3ad1329622933c
NadezdaSatohina <124875434+NadezdaSatohina@users.noreply.github.com>
2023-03-02 17:04:58 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -3,52 +3,16 @@
 import sys
 import threading
 
+
+
 def compute_height(n, parents):
-    adj_list = [[] for _ in range(n)]
-    for i, p in enumerate(parents):
-        if p != -1:
-            adj_list[p].append(i)
-
+    # Write this function
     max_height = 0
-    stack = [(None, parents.index(-1), 0)]
-    while stack:
-        parent, node, height = stack.pop()
-        if height > max_height:
-            max_height = height
-        for child in adj_list[node]:
-            if child != parent:
-                stack.append((node, child, height + 1))
-
+    # Your code here
     return max_height
 
+
 def main():
-    input_type = input()
-
-    if "F" in input_type:
-        filename = input()
-        if "a" in filename or "A" in filename:
-            return
-        if not filename.startswith("test/"):
-            filename = "test/" + filename
-        with open(filename) as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().split()))
-            height = compute_height(n, parents)
-    elif "I" in input_type:
-        n = int(input())
-        parents = list(map(int, input().split()))
-        height = compute_height(n, parents)
-
-    print(height)
-
-#def compute_height(n, parents):
-    # Write this function
-    #max_height = 0
-    # Your code here
-    #return max_height
-
-
-#def main():
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
