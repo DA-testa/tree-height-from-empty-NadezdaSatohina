57b62b92e4396c0d55080e7032c2255d921a47bb
NadezdaSatohina <124875434+NadezdaSatohina@users.noreply.github.com>
2023-03-02 09:12:48 +0000

    test1

--- tree_height.py
+++ tree_height.py
@@ -3,15 +3,30 @@
 import sys
 import threading
 
+tree_len = int(input("Node Count:"))
+input_arr = [int(node) for node in input("Nodes: ").split(" ")]
 
-def compute_height(n, parents):
+def get_child_nodes(nodes_to_find, initial_input, level):
+    level += 1
+    child_nodes = []
+    for node in nodes_to_find: #[0] -> [4] -> [1] -> [2, 5]
+        for index, pointer in enumerate(initial_input): #[(0, -1), (1, 3), (2, 1), (3, 0), (4, 1), (5, 2)]
+            if pointer == node:
+                child_nodes.append(index)
+    if child_nodes:
+        level = get_child_nodes(child_nodes, initial_input, level)
+    return level
+    
+result = get_child_nodes([input_arr.index(-1)], input_arr, 0)
+print(result)
+#def compute_height(n, parents):
     # Write this function
-    max_height = 0
+    #max_height = 0
     # Your code here
-    return max_height
+    #return max_height
 
 
-def main():
+#def main():
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
